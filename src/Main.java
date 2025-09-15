c8c146307bbc76876b34b3af887b47b4482f35de
Caleb Henry <115678147+cablebIll@users.noreply.github.com>
2025-09-15 14:07:20 +1000

    week 02 Worksheet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        System.out.print("~~~~~\nCOMP2000 2025\n~~~~~~");
     }
 }
