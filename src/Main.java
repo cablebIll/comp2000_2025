ece606eb191db1cfd8507e869c402e0d8cbb7ae8
Caleb Henry <115678147+cablebIll@users.noreply.github.com>
2025-09-15 14:09:06 +1000

    Revert "week 3 task 3 and 4"
    This reverts commit b7009a102218d5ba093c22a5ab4e552c0e795345.

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,15 @@ public class Main extends JFrame {
     }
 
     class Canvas extends JPanel {
-      Grid grid = new Grid();
       public Canvas() {
-        setPreferredSize(new Dimension(720, 720)); //set the canvas size in 720 x 720
+        setPreferredSize(new Dimension(720, 720));
       }
-
 
       @Override
       public void paint(Graphics g) {
-        grid.paint(g, getMousePosition());
+	g.setColor(java.awt.Color.BLACK);
+	g.drawRect(10, 10, 700, 700);
       }
-
     }
 
     private Main() {
