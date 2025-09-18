4f01dc782aab17b3b6b07bebb40634fded9d4157
Caleb Henry <115678147+cablebIll@users.noreply.github.com>
2025-09-18 14:10:23 +1000

    task 6 solution

--- src/Cell.java
+++ src/Cell.java
@@ -1,17 +1,15 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
 
-public class Cell {
+public class Cell extends Rectangle {
   // fields
-  int x;
-  int y;
   static int size = 35;
 
   // constructors
-  public Cell(int inX, int inY) {
-    x = inX;
-    y = inY;
+  public Cell(int x, int y) {
+    super(x, y, size, size);
   }
 
   // methods
@@ -27,10 +25,9 @@ public class Cell {
   }
 
   public boolean contains(Point p) {
-    if(p != null) {
-      return x < p.x && x+size > p.x && y < p.y && y+size > p.y;
-    } else {
-      return false;
-    }
-  }
+        if(p != null) {
+    return super.contains(p);
+        }
+        else {return false;}
+      }
 }
