cb24afce165c108bc651a44d5dcc7a8a13146e85
Caleb Henry <115678147+cablebIll@users.noreply.github.com>
2025-09-18 14:55:28 +1000

    Revert "task 10 solution."
    This reverts commit bbbbab7e00000c2660734bfc8bdcf5eb7f28161f.

--- src/Stage.java
+++ src/Stage.java
@@ -1,24 +1,23 @@
 import java.awt.Graphics;
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Stage {
   Grid grid;
-  List<Actor> actors;
+  Actor cat;
+  Actor dog;
+  Actor bird;
 
   public Stage() {
     grid = new Grid();
-    actors = new ArrayList<Actor>();
-    actors.add(new Cat(grid.cellAtColRow(0, 0)));
-    actors.add(new Dog(grid.cellAtColRow(0, 15)));
-    actors.add(new Bird(grid.cellAtColRow(12, 9)));
+    cat = new Cat(grid.cellAtColRow(0, 0));
+    dog = new Dog(grid.cellAtColRow(0, 15));
+    bird = new Bird(grid.cellAtColRow(12, 9));
   }
 
   public void paint(Graphics g, Point mouseLoc) {
     grid.paint(g, mouseLoc);
-    for(Actor a: actors) {
-      a.paint(g);
-    }
+    cat.paint(g);
+    dog.paint(g);
+    bird.paint(g);
   }
 }
