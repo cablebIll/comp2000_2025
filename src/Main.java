701c8053a076308121759505e801e40313ec4dab
Caleb Henry <115678147+cablebIll@users.noreply.github.com>
2025-09-22 22:17:59 +1000

    made the components of main safer

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,66 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
-
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import javax.swing.Timer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Main extends JFrame {
-    public static void main(String[] args) throws Exception {
-      Main window = new Main();
-      window.run();
+  private final Canvas canvas;
+
+  public static void main(String[] args) throws Exception {
+    Main window = new Main();
+    window.run();
+  }
+
+  class Canvas extends JPanel implements KeyListener {
+    Stage stage = new Stage();
+
+    public Canvas() {
+      setPreferredSize(new Dimension(1024, 720));
+      this.addKeyListener(this);
+      this.setFocusable(true);
+      this.requestFocusInWindow();
     }
 
-    class Canvas extends JPanel implements KeyListener {
-      Stage stage = new Stage();
-      public Canvas() {
-        setPreferredSize(new Dimension(1024, 720));
-        this.addKeyListener(this);
-        this.setFocusable(true);
-        this.requestFocusInWindow();
-      }
-
-      @Override
-      public void keyPressed(KeyEvent e) {
-        System.out.print(e.getKeyCode());
-        stage.handleInput(e.getKeyCode()); 
-      }
-
-      @Override
-      public void keyTyped(KeyEvent e) {
-      }
-
-      @Override
-      public void keyReleased(KeyEvent e) {
-      }
-
-      @Override
-      public void paint(Graphics g) {
-        stage.paint(g, getMousePosition());
-      }
-    }
-
-    private Main() {
-      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Canvas canvas = new Canvas();
-      this.setContentPane(canvas);
-      this.pack();
-      this.setVisible(true);
-    }
-
-    public void run() {
-  Timer timer = new Timer(1000 / 60, new ActionListener() {
     @Override
-    public void actionPerformed(ActionEvent e) {
-      ((Canvas) getContentPane().getComponent(0)).stage.update();
-      getContentPane().getComponent(0).repaint();
-
+    public void keyPressed(KeyEvent e) {
+      stage.handleInput(e.getKeyCode());
     }
-  });
-  timer.start();
-}
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void paint(Graphics g) {
+      stage.paint(g, getMousePosition());
+    }
+  }
+
+  private Main() {
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.canvas = new Canvas();
+    this.setContentPane(canvas);
+    this.pack();
+    this.setVisible(true);
+  }
+
+  public void run() {
+    Timer timer = new Timer(1000 / 60, new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        canvas.stage.update();
+        canvas.repaint();
+      }
+    });
+    timer.start();
+  }
 }
